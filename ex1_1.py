fd501c09cfd18e104093487e128766a685fc92fb
Vishchdok <chris.mappus@gmail.com>
2019-04-17 19:44:40 -0500

    Finished exercise 1

--- ex1_1.py
+++ ex1_1.py
@@ -6,3 +6,6 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 bs = BeautifulSoup(html.read(), 'html.parser')
 print(bs.h1)
+
+# <h1>An Interesting Title</h1>
+# Note that this prints the first instance of an h1 tag found on the page.
